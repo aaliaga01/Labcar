42399254631d152aa455e497cc408c06f623cbf7
aaliaga-casa <a.aliaga.val@gmail.com>
2017-06-21 20:53:25 -0300

    seccion tarifas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,12 @@
 /*Geolocalizacion*/
 
 function initMap(){
-  //Para que el mapa aparezca en el sitio
 
-  var directionsService = new google.maps.DirectionsService;
-  var directionsDisplay = new google.maps.DirectionsRenderer;
+  //Para que el mapa aparezca en el sitio
   var map = new google.maps.Map(document.getElementById('mapa'), {
     zoom: 12,
-    center: {lat: -33.4569400, lng: -70.6482700}//mapa parte en santiago
+    center: {lat: -33.4569400, lng: -70.6482700},//mapa parte en santiago
+    disableDefaultUI: true //para contoles de zoom y imagen relieve - satelite NO se muestren
   });
   
-  directionsDisplay.setMap(map);
 }
