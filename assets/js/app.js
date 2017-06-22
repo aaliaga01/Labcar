b7f3b3cb714354965ef727c0d663f8e946430862
aaliaga-casa <a.aliaga.val@gmail.com>
2017-06-22 01:19:54 -0300

    responsive terminado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,42 @@
 /*Geolocalizacion*/
 
-function initMap(){
+function initMap() {
+        var map = new google.maps.Map(document.getElementById('mapa'), {
+          center: {lat: -33.4569400, lng: -70.6482700}, //muestra ubicacion inicial en stgo
+          zoom: 17,
+          disableDefaultUI: true
+        });
 
-  //Para que el mapa aparezca en el sitio
-  var map = new google.maps.Map(document.getElementById('mapa'), {
-    zoom: 12,
-    center: {lat: -33.4569400, lng: -70.6482700},//mapa parte en santiago
-    disableDefaultUI: true //para contoles de zoom y imagen relieve - satelite NO se muestren
-  });
-  
-}
+        // Pregunta si quieres activar geolocalizacion.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var miPosicion = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            map.setZoom(17);
+    		map.setCenter(miPosicion); //centra el mapa en la ubicacion
+
+            var marker = new google.maps.Marker({ //funcion que coloca un marcador
+        	position: miPosicion,
+        	animation: google.maps.Animation.BOUNCE,
+        	map: map,
+    		});
+
+        	}, 
+
+          	function() {
+         	error(true, map.getCenter());
+          	});
+
+        } else {
+          // se ejecuta esta funcion si no escuentra la ubicacion
+          error(false, map.getCenter());
+        }
+}// fin de funcion initMap
+
+      //funcion para mensaje de error
+      function error() {
+        alert("No es posible encontrar tu ubicacion");
+      }
